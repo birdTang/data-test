f560dd3a82730724420e32b84979d2fd05a935e1
tangpeng <tangpeng@0easy.com>
2018-11-13 18:42:26 +0800

    添加排序代码

---
+++ src/sort/MergeSort.java
@@ -0,0 +1,77 @@
+package sort;
+
+/**
+ * 归并排序
+ * @ate 2018年11月13日
+ * @author Administrator
+ */
+public class MergeSort {
+
+	/**
+	 * 
+	 * @date 2018年11月13日
+	 * @author tangp
+	 * @param arr 数组
+	 * @param n 数组长度
+	 */
+	private static void mergeSort(int[] arr,int n) {
+		mergeSortInternally(arr, 0, n-1);
+		System.out.println(SortMethod.printVal(arr));
+	}
+	
+	private static void mergeSortInternally(int[] a,int p,int r) {
+		//递归终止条件
+		if(p>=r) return;
+		//取p到r之间的中间位置q,防止（p+r）的和超过int类型最大值
+		int q = p + (r-p)/2;
+		
+		//分治递归
+		mergeSortInternally(a, p, q);
+		mergeSortInternally(a, q+1, r);
+		
+		//将A[p...q]和A[q+1...r]合并为A[p...r]
+		merge(a, p, q, r);
+		
+	}
+	
+	private static void merge(int[] a,int p ,int q,int r) {
+		int i=p;  //分治后，左边数组当前下标
+		int j=q+1; //分治后，右边数组当前下标
+		int k=0;  //临时数组开始当前下标
+		
+		int[] tmp = new int[r-p+1];
+
+		while(i<=q && j<=r) {
+			if(a[i]<=a[j]) {
+				tmp[k++] = a[i++];
+			}else {
+				tmp[k++] = a[j++];
+			}
+		}
+		
+		//判断哪个子数组有剩余的数组
+		int start = i;
+		int end = q;
+		if(j<=r) {
+			start = j;
+			end = r;
+		}
+		
+		//将剩余的数组copy到tmp数组中
+		while(start<=end) {
+			tmp[k++] = a[start++];
+		}
+		
+		//将tmp数组拷贝回a[p.....r]
+		for(i=0;i<=r-p;i++) {
+			a[p+i] = tmp[i];
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		int[] arr = {3, 2, 6, 4, 5, 1, 9, 20, 13, 16};
+		
+		mergeSort(arr, arr.length);
+	}
+}
---
+++ src/sort/QuickSort.java
@@ -0,0 +1,51 @@
+package sort;
+
+/**
+ * 快速排序
+ * @ate 2018年11月13日
+ * @author Administrator
+ */
+public class QuickSort {
+
+	private static void quickSort(int[] a,int n) {
+		quickSortInternally(a, 0, n-1);
+		System.out.println(SortMethod.printVal(a));
+	}
+	
+	private static void quickSortInternally(int[] a,int p,int r) {
+		//递归终止条件
+		if(p>=r) return;
+		int q = partition(a, p, r); //获取分区点
+		quickSortInternally(a, p, q-1);
+		quickSortInternally(a, q+1, r);
+	}
+	
+	private static int partition(int[] a,int p,int r) {
+		
+		int pivot = a[r];
+		int i = p;
+		for(int j=p;j<=r;j++) {
+			//小于pivot，则交换
+			if(a[j] < pivot) {
+				int tmp = a[i];
+				a[i] = a[j];
+				a[j] = tmp;
+				++i;
+			}
+		}
+		
+		int tmp = a[i];
+		a[i] = a[r];
+		a[r] = tmp;
+		
+		 System.out.println("i=" + i);
+		 return i;
+		
+	}
+	
+	public static void main(String[] args) {
+		int[] arr = {3, 2, 6, 4, 5, 1, 9, 20, 13, 16};
+		
+		quickSort(arr, arr.length);
+	}
+}
